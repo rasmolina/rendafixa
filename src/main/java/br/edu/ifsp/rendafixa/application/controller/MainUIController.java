2602822e8a069f31d186ef9a11f419a4a61a2723
Lucas Selin <lucas.selin@aluno.ifsp.edu.br>
2023-06-14 02:09:46 -0300

    adicionando funcionalidade na main e edicao no app

--- src/main/java/br/edu/ifsp/rendafixa/application/controller/MainUIController.java
+++ src/main/java/br/edu/ifsp/rendafixa/application/controller/MainUIController.java
@@ -1,21 +1,31 @@
 package br.edu.ifsp.rendafixa.application.controller;
 
+import br.edu.ifsp.rendafixa.application.view.WindowLoader;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
 
 public class MainUIController {
-    public void moveToEmissoraManagement(ActionEvent actionEvent) {
+
+    public void moveToEmissoraManagement(ActionEvent actionEvent) throws IOException {
+        WindowLoader.setRoot("EmissoraManagementUI");
     }
 
-    public void moveToAtivoManagement(ActionEvent actionEvent) {
+    public void moveToAtivoManagement(ActionEvent actionEvent) throws IOException {
+        WindowLoader.setRoot("AtivoManagementUI");
     }
 
-    public void moveToCarteiraManagement(ActionEvent actionEvent) {
+    public void moveToCarteiraManagement(ActionEvent actionEvent) throws IOException {
+        WindowLoader.setRoot("CarteiraManagementUI");
     }
 
-    public void moveToPortadoraManagement(ActionEvent actionEvent) {
+    public void moveToPortadoraManagement(ActionEvent actionEvent) throws IOException {
+        WindowLoader.setRoot("PortadoraManagementUI");
     }
 
-    public void moveToIndexadorManagement(ActionEvent actionEvent) {
+    public void moveToIndexadorManagement(ActionEvent actionEvent) throws IOException {
+        WindowLoader.setRoot("IndexadorManagementUI");
     }
 
     public void moveToTransacoes(ActionEvent actionEvent) {
